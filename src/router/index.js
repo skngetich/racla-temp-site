131541a36c761d1bb087f36495cec2d2183190f3
Stephen Ng'etich <stephenngetich5@gmail.com>
2021-07-27 00:25:19 +0300

    updated document title and removes service worker files

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,20 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: "Rotaract Lang'ata",
+    },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "Rotaract Lang'ata"
+  next()
 })
 
 export default router
