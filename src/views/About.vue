c221525905d8ee002adb96aec40b19394bbbd76c
Stephen Ng'etich <stephenngetich5@gmail.com>
2021-07-26 22:43:12 +0300

    init commit updatte

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an about page for a vue project</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center">
+    <img
+      alt="Vue logo"
+      class="mx-auto animate-bounce"
+      src="../assets/logo.png"
+    />
+    <p class="text-3xl text-gray-900">Well the templates works :)</p>
+    <p class="text-base mb-4 text-green-600">Build something great now</p>
+
+    <p class="text-base text-green-600">
+      if you encounter any issues with the template, please raise the
+      <a
+        class="text-gray-900 underline"
+        href="https://github.com/ilabafrica-IoTlab/vue3-starter-template/issues"
+      >
+        issue on github</a
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {},
+}
+</script>
